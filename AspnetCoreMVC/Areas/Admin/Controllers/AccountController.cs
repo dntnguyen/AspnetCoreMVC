0ab93de4993162424aed710d31d2ffed8a5a27af
Duong Nguyen The Nguyen <dntnguyen@outlook.com>
2020-02-27 11:46:36 +0700

    Login Partial Logout

---
+++ AspnetCoreMVC/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreApp.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCoreMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : BaseController
+    {
+        SignInManager<AppUser> _signInManager;
+        public AccountController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Redirect("/Admin/Login/Index");
+        }
+    }
+}
